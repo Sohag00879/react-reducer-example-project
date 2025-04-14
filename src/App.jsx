3ea5490a94a25a5dbecc406125f7dc62c1e4b19b
sohag <shishirsohagislam@gmail.com>
2025-04-14 16:04:52 +0600

    added task

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import TravelPlan from "./components/TravelPlan";
+import { useState } from 'react';
+import AddTask from "./components/AddTask";
+import TaskList from "./components/TaskList";
+import { initialTasks } from './data/tasks';
 
 export default function App() {
-  return (
-    <div>
-      <TravelPlan />
-    </div>
-  );
+    const [tasks, setTasks] = useState(initialTasks)
+    return (
+        <>
+            <h1>Prague itinerary</h1>
+            <AddTask />
+            <TaskList tasks={tasks} />
+
+        </>
+    );
 }
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,8 @@
+export default function AddTask() {
+    return (
+        <>
+            <input type="Add task" />
+            <button>Add</button>
+        </>
+    );
+}
